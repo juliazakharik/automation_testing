17cd5e521bf13476f5ceae8d3e5cb7d41e08dc23
Zakharik Julia <zakharik.julia@gmail.com>
2019-12-17 19:29:40 +0300

    upd junit

--- ci/src/test/java/AppTest.java
+++ ci/src/test/java/AppTest.java
@@ -1,8 +1,8 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-class AppTest {
+public class AppTest {
     @Test
     void test(){
         assertTrue(true);
